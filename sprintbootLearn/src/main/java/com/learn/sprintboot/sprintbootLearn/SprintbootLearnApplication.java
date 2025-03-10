e234988b1340d116351c0b47e8f93a3ebd86a9fa
akshay2815 <shelkeakshay222@gmail.com>
2025-03-10 22:55:26 +0530

    Latest

---
+++ sprintbootLearn/src/main/java/com/learn/sprintboot/sprintbootLearn/SprintbootLearnApplication.java
@@ -0,0 +1,13 @@
+package com.learn.sprintboot.sprintbootLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootLearnApplication.class, args);
+	}
+
+}
---
+++ sprintbootLearn/src/test/java/com/learn/sprintboot/sprintbootLearn/SprintbootLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.sprintboot.sprintbootLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
